6b492c1e7b4d860c86eed7ee5d1e2f873d8e0e9e
Paul Venhaus <paul.venhaus@gmail.com>
2017-11-16 20:17:01 +0000

    Implement model validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ApplicationRecord
+  MIN_DURATION = 5
+  MAX_DURATION = 30
+
   belongs_to :user
+  enum category: %i[event news]
+
+  validates :title, :content, :category, :duration, :expires_on, presence: true
+  validates :duration, inclusion: { in: MIN_DURATION..MAX_DURATION }
+  validate :expiry_cannot_be_in_past, on: %i[create update]
+
+  def expiry_cannot_be_in_past
+    errors.add(:expires_on, ' cannot be in the past!') if expires_on < Time.now
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   enum role: %i[admin staff]
 
   has_many :posts
+
+  validates :email, :password, :name, :role, presence: true
 end
