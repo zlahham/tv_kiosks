0bf6b899c3896ff08056ab9297163159bd0a862f
Paul Venhaus <paul.venhaus@gmail.com>
2017-11-16 18:03:30 +0000

    Add background; add authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!, unless: -> { devise_controller? || is_a?(KioskController) }
 end
