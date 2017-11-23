211c3523449de4f8223d193d2323d70efd0c6749
Paul Venhaus <paul.venhaus@gmail.com>
2017-11-23 17:13:31 +0000

    Use jQuery to access DOM and improve hour & minute formatting

--- app/assets/javascripts/date_time.js
+++ app/assets/javascripts/date_time.js
@@ -1,15 +1,22 @@
 function startTimeAndDate() {
     var today = new Date();
-    var hour = today.getHours();
-    var minute = today.getMinutes();
+    var hour = formatTime(today.getHours());
+    var minute = formatTime(today.getMinutes());
     var day = today.getDate();
     var month = today.getMonth() + 1;
     var year = today.getFullYear();
-    document.getElementById('div-time').innerHTML = hour + ':' + minute;
-    document.getElementById('div-date').innerHTML = day + '/' + month + '/' + year;
+    $('#div-time')[0].innerHTML = hour + ':' + minute;
+    $('#div-date')[0].innerHTML = day + '/' + month + '/' + year;
     var t = setTimeout(startTimeAndDate, 500);
 }
 
-$( document ).ready(function() {
+function formatTime(time) {
+  if (time < 10) {
+    time = '0' + time;
+  }
+  return time;
+}
+
+$(document).ready(function() {
   startTimeAndDate();
 });
