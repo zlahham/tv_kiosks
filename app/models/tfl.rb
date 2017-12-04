58e921933965ca288a82d1dbe52def88ce062058
Paul Venhaus <paul@ven.haus>
2017-12-04 17:46:34 +0000

    Move tfl api key and app id to bash profile and clean up view code

--- app/models/tfl.rb
+++ app/models/tfl.rb
@@ -1,15 +1,15 @@
 require 'httparty'
 
 class TFL
-  url = 'https://api.tfl.gov.uk/line/mode/tube/status?app_id=00dbaeee&app_key=780a63dac025fb52d24aa41d645cd962'
+  url = "https://api.tfl.gov.uk/line/mode/tube/status?app_id=#{Rails.application.secrets.tfl_app_id}&app_key=#{Rails.application.secrets.tfl_api_key}"
   response = HTTParty.get(url)
   all_disruptions = response.parsed_response
 
-  @disruptions = {}
+  @@disruptions = {}
 
   all_disruptions.each do |line|
     if line['lineStatuses'][0]['statusSeverity'] < 10
-      @disruptions[line['id']] =
+      @@disruptions[line['id']] =
         line['lineStatuses'][0]['statusSeverityDescription'] +
         " on #{line['name']} line."
     end
@@ -17,9 +17,14 @@ class TFL
 
   def self.disruptions
     list = []
-    @disruptions.each_with_index do |disruption, index|
+    @@disruptions.each_with_index do |disruption, index|
       list[index] = disruption[1]
     end
     return list
+  end
+
+  def self.no_disruptions
+    disruption_list = self.disruptions
+    return disruption_list.blank?
   end
 end
