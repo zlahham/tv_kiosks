de334e01799f279760ca32dcb50596168277d4f6
Zaid Al Lahham <zlahham@gmail.com>
2017-12-04 22:24:36 +0000

    Update the Views for the Rails admin

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,4 +12,39 @@ class Post < ApplicationRecord
   def expiry_cannot_be_in_past
     errors.add(:expires_on, ' cannot be in the past!') if expires_on < Time.now
   end
+
+  rails_admin do
+    edit do
+      group :default do
+        label 'Post information'
+        help 'Please fill all information related to your post...'
+      end
+    end
+
+    list do
+      field :title
+      field :user
+      field :category
+      field :date
+      field :duration
+      field :expires_on
+
+      group :default do
+        field :user do
+          label 'Author'
+        end
+        field :title do
+          label 'Post Title'
+        end
+      end
+    end
+
+    object_label_method do
+      :post_label_method
+    end
+  end
+
+  def post_label_method
+    "Post ##{id}"
+  end
 end
