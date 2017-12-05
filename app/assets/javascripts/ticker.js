b2300eec276a11911d683b43d295df360b2410aa
Aleksi Anttila <aleksi.anttila303@gmail.com>
2017-12-05 18:10:54 +0000

    Change to a ticker that scrolls infinitely

--- app/assets/javascripts/ticker.js
+++ app/assets/javascripts/ticker.js
@@ -1,5 +1,22 @@
 $( document ).ready(function() {
   console.log( "ready!" );
+
+  // jQuery marquee
+  $('.marquee').marquee({
+	allowCss3Support: true,
+	css3easing: 'linear',
+	// note: the other options affect speed as well
+	speed: 100,
+	//time in milliseconds before the marquee will start animating
+	delayBeforeStart: 0,
+	direction: 'left',
+	//true or false - should the marquee be duplicated to show an effect of continuous flow
+	duplicated: false,
+	//gap in pixels between the tickers (only works when duplicated is true)
+	gap: 50
+  });
+
+  // jQuery ticker; currently not in use
   $.fn.ticker.defaults = {
     random:        false, // Whether to display ticker items in a random order
     itemSpeed:     3000,  // The pause on each ticker item before being replaced
@@ -13,4 +30,5 @@ $( document ).ready(function() {
     fadeOutSpeed:  800    // Speed of the fade-out animation
   };
   $('.ticker').ticker();
+
 });
