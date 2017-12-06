9d7b01e9949445b7c51b741bf5798724c60ea8a7
Zaid Al Lahham <zlahham@gmail.com>
2017-12-06 18:18:09 +0000

    Further TFL Cleanup

--- app/models/tfl.rb
+++ app/models/tfl.rb
@@ -1,28 +1,37 @@
-require 'httparty'
-
 class TFL
-  url = "https://api.tfl.gov.uk/line/mode/tube/status?app_id=#{Rails.application.secrets.tfl_app_id}&app_key=#{Rails.application.secrets.tfl_api_key}"
-  response = HTTParty.get(url)
-  all_disruptions = response.parsed_response
+  URL = 'https://api.tfl.gov.uk/line/mode/tube/status?'\
+        "app_id=#{Rails.application.secrets.tfl_app_id}&"\
+        "app_key=#{Rails.application.secrets.tfl_api_key}".freeze
 
-  @@disruptions = {}
+  class << self
+    @@response = HTTParty.get(URL).parsed_response
+    @@disruptions = {}
 
-  all_disruptions.each do |line|
-    if line['lineStatuses'][0]['statusSeverity'] < 10
-      @@disruptions[line['id']] =
-        line['name'] + ' Line: ' + line['lineStatuses'][0]['statusSeverityDescription']
+    def disruptions
+      @@response.each do |line|
+        next unless bad_service?(line)
+        @@disruptions[line['id']] = "#{line_name(line)} Line: #{line_status(line)}"
+      end
+
+      @@disruptions.each_with_index.inject([]) do |acc, (disruption, index)|
+        acc[index] = disruption[1]
+      end
     end
-  end
 
-  def self.disruptions
-    list = []
-    @@disruptions.each_with_index do |disruption, index|
-      list[index] = disruption[1]
+    def no_disruptions?
+      disruptions.blank?
     end
-    list
-  end
 
-  def self.no_disruptions?
-    disruptions.blank?
+    def bad_service?(line)
+      line['lineStatuses'][0]['statusSeverity'] < 10
+    end
+
+    def line_name(line)
+      line['name']
+    end
+
+    def line_status(line)
+      line['lineStatuses'][0]['statusSeverityDescription']
+    end
   end
 end
