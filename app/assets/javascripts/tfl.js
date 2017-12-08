60ad49c3a575024d9254d562adc46ac77adacd6c
Paul Venhaus <paul@ven.haus>
2017-12-08 18:10:13 +0000

    Fix color display on safari

--- app/assets/javascripts/tfl.js
+++ app/assets/javascripts/tfl.js
@@ -5,14 +5,18 @@ function paintTFLColors() {
     for (var index in lineArray) {
         var line = lineArray[index];
         paintTFLIndicatorHelper(line);
-        console.log(line);
     }
+
 }
 
 function paintTFLIndicatorHelper(line) {
     var tflIndicator = document.getElementById('tfl-indicator');
     if ($('#tfl-widget').find('.carousel-item.active').text().indexOf(line) >= 0) {
         tflIndicator.setAttribute("class", line);
+        // Needed to force CSS repaint on Safari
+        tflIndicator.style.display='none';
+        tflIndicator.offsetHeight; // no need to store this anywhere, the reference is enough
+        tflIndicator.style.display='';
     }
 }
 
