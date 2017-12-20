59912036bf3d3904fbf87541dff1ff43d504799b
Zaid Al Lahham <zlahham@gmail.com>
2017-12-20 23:05:46 +0200

    Clarify numbers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,9 @@
 //= require_tree .
 
 $(function() {
+  var refreshRate = 1800000; // In Milliseconds
+
   setTimeout(function() {
       location.reload();
-  }, 1800000);
+  }, refreshRate);
 });
