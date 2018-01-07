f403eca289af7155d390beca8028e7bb5deb538d
Zaid Al Lahham <zlahham@gmail.com>
2018-01-07 18:43:57 +0000

    Add colour hex validation

--- app/models/department.rb
+++ app/models/department.rb
@@ -3,4 +3,6 @@ class Department < ApplicationRecord
 
   validates :colour, :name, :code, presence: true
   validates :code, uniqueness: true
+
+  validates :colour, format: { with: /(?<=#)(?<!^)\h{6}/, message: 'only allows colour hexidecimal e.g. #6D979A' }
 end
