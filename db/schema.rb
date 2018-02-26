9ab3ce88c5f5a931326d17ed21427a5acad92233
Alex Forey <me@alexforey.com>
2018-02-26 21:46:27 +0000

    Added twitter display to model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180107185148) do
+ActiveRecord::Schema.define(version: 20180226212922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180107185148) do
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "twitter_handle"
   end
 
   create_table "posts", force: :cascade do |t|
