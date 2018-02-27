c2daae906e0b7434cd250ff119f66cc96a81a222
Alex Forey <me@alexforey.com>
2018-02-27 13:12:29 +0000

    Added schema for screens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226212922) do
+ActiveRecord::Schema.define(version: 20180227130744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 20180226212922) do
     t.integer "attachment_file_size"
     t.datetime "attachment_updated_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "screens", force: :cascade do |t|
+    t.bigint "department_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["department_id"], name: "index_screens_on_department_id"
+    t.index ["name"], name: "index_screens_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
