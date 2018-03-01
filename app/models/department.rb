91d338d85ed7383d0b59d56c222958bbd1abfbc2
Alex Forey <me@alexforey.com>
2018-03-01 11:57:31 +0000

    Fix database and seed issue creating new departments

--- app/models/department.rb
+++ app/models/department.rb
@@ -4,7 +4,7 @@ class Department < ApplicationRecord
 
   validates :colour, :name, :code, presence: true
   validates :code, uniqueness: true
-  validates :twitter_handle, format: { with: /\A[A-Za-z0-9_]{1,15}\z/, message: 'must be a valid Twitter username'}
+  validates :twitter_handle, format: { with: /\A[A-Za-z0-9_]{1,15}\z/, message: 'must be a valid Twitter username'}, allow_blank: true
 
   validates :colour, format: { with: /(?<=#)(?<!^)\h{6}/, message: 'only allows colour hexidecimal e.g. #6D979A' }
 end
