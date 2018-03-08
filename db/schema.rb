0bda1ad9b2178dfa4611bb0af6889310347f9f23
gohchanb <32335285+gohchanb@users.noreply.github.com>
2018-03-08 11:40:57 +0000

    Added video_url column to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227133651) do
+ActiveRecord::Schema.define(version: 20180308111403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20180227133651) do
     t.string "attachment_content_type"
     t.integer "attachment_file_size"
     t.datetime "attachment_updated_at"
+    t.string "video_url"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
