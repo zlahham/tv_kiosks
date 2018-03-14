c9668c363b3bb943a1d59d6c3d904acf2279b341
gohchanb <32335285+gohchanb@users.noreply.github.com>
2018-03-14 15:16:01 +0000

    added javascript for video play on rotation

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -2,6 +2,18 @@
 // the previous post is made invisible using prevMainIndex.
 var mainIndex = 0;
 var prevMainIndex = 0;
+var loopedOnce = 0;
+
+//This function handles the youtube embedded video being paused while 
+function toggleVideo(state, div) {
+    // if state == 'hide', hide. Else: show video
+    //var div = document.getElementByClassName("video-post");
+    var iframe = div.find("iframe")[0].contentWindow;
+    //div.style.display = state == 'hide' ? 'none' : '';
+    func = state == 'hide' ? 'pauseVideo' : 'playVideo';
+    iframe.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
+    iframe.postMessage('{"event":"command","func":"' + func + '","args":""}', '*');
+}
 
 /**
  * This function handles the switching of available posts and category tabs in the kiosk if there are more
@@ -16,6 +28,12 @@ function switchPosts() {
 
     // Only switches posts if there are more than 1 post
     if (posts.length > 1) {
+        //If post is a video play it
+        if ($($(posts).get(mainIndex)).hasClass('video-post'))
+        {
+            toggleVideo('show',$($(posts).get(mainIndex)));
+        }
+
         // Places the post with number mainIndex at the top and moves the prevMainIndex post to the back
         $($(posts).get(mainIndex)).css("z-index", 1);
         $($(posts).get(prevMainIndex)).css("z-index", 0);
@@ -29,6 +47,10 @@ function switchPosts() {
 
         // After the transition is finished, the previously displayed post is hidden
         setTimeout(function () {
+            if ($($(posts).get(prevMainIndex - 1)).hasClass('video-post'))
+            {
+                toggleVideo('hide',$($(posts).get(prevMainIndex - 1)));
+            }
             $($(posts).get(prevMainIndex - 1)).hide();
             $($(tabs).get(prevMainIndex - 1)).hide();
         }, transitionTime);
@@ -36,6 +58,7 @@ function switchPosts() {
         // Increments prevMainIndex and mainIndex while ensuring that prevMainIndex is always
         // 1 behind mainIndex and that both restart at 0 after reaching the total number of posts
         if (mainIndex >= posts.length - 1) {
+            loopedOnce = 1;
             mainIndex = 0;
             prevMainIndex = posts.length -1;
         } else {
