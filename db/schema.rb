741cd9f9788f1b71490c992e1fea5d208d97eee4
Alex Forey <me@alexforey.com>
2018-03-20 13:20:01 +0000

    Fixed empty twitter display bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308111403) do
+ActiveRecord::Schema.define(version: 20180308113624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180308111403) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "twitter_handle"
+    t.string "instagram_account"
   end
 
   create_table "posts", force: :cascade do |t|
